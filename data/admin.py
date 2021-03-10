81e9d66aae18a05ebc914f308f722dec6cfe7583
Leo Suarez <leo@dtraleigh.com>
2021-03-10 09:05:58 -0500

    Created models and imported initial data.

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from data.models import *
 
-# Register your models here.
+
+class WaterAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Water._meta.get_fields()]
+
+
+class ElectricityAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Electricity._meta.get_fields()]
+
+
+class GasAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Gas._meta.get_fields()]
+
+
+admin.site.register(Water, WaterAdmin)
+admin.site.register(Electricity, ElectricityAdmin)
+admin.site.register(Gas, GasAdmin)
--- data/models.py
+++ data/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Water(models.Model):
+    submit_date = models.DateField(auto_now_add=True)
+    bill_date = models.DateField(blank=True, null=True)
+    service_start_date = models.DateField()
+    service_end_date = models.DateField()
+    avg_gallons_per_day = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name_plural = "Water"
+
+    def __str__(self):
+        return "Water %s-%s (%s)" % (self.service_start_date,
+                                     self.service_end_date,
+                                     self.id)
+
+
+class Electricity(models.Model):
+    submit_date = models.DateField(auto_now_add=True)
+    bill_date = models.DateField(blank=True, null=True)
+    service_start_date = models.DateField()
+    service_end_date = models.DateField()
+    kWh_usage = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Electricity"
+
+    def __str__(self):
+        return "Water %s-%s (%s)" % (self.service_start_date,
+                                     self.service_end_date,
+                                     self.id)
+
+
+class Gas(models.Model):
+    submit_date = models.DateField(auto_now_add=True)
+    bill_date = models.DateField(blank=True, null=True)
+    service_start_date = models.DateField()
+    service_end_date = models.DateField()
+    therms_usage = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Gas"
+
+    def __str__(self):
+        return "Water %s-%s (%s)" % (self.service_start_date,
+                                     self.service_end_date,
+                                     self.id)
