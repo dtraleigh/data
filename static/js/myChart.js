a4adf13d20d1071676bfaf7195114c8e547354a5
Leo Suarez <leo@dtraleigh.com>
2021-03-21 08:31:22 -0400

    Edited the point tooltips to display the y value only.

--- static/js/myChart.js
+++ static/js/myChart.js
@@ -1,25 +1,30 @@
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
-type: 'scatter',
-data: chart_data,
-options: {
-  scales: {
-    yAxes: [{
-      ticks: {
-        beginAtZero: false
-      }
-    }],
-    xAxes: [{
+  type: 'scatter',
+  data: chart_data,
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }],
+      xAxes: [{
         type: 'time',
         time: {
-            unit: 'month'
+          unit: 'month'
         }
-    }]
-  },
-  legend: {
-    display: true,
-  },
-  tooltipTemplate: "<%if (label){%><%=label %>: <%}%><%= value + ' %' %>",
-  multiTooltipTemplate: "<%= value + ' %' %>",
-}
+      }]
+    },
+    legend: {
+      display: true,
+    },
+    tooltips: {
+      callbacks: {
+        label: function(tooltipItem, data) {
+          return tooltipItem.yLabel;
+        },
+      }
+    }
+  }
 });
