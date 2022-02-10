4c26e667785c8ddbae1c3acac36ffc8e91abd1ce
Leo Suarez <leo@dtraleigh.com>
2022-02-10 09:44:16 -0500

    Created create_line_data() and moved all functions into functions.py

--- data/views.py
+++ data/views.py
@@ -1,111 +1,11 @@
 from django.shortcuts import render
-from django.apps import apps
 from data.models import *
 import logging
 from datetime import datetime
 
-colors = ["rgba(0, 200, 0, 1)",  # Green
-          "rgba(200, 0, 0, 1)",  # Red
-          "rgba(191, 63, 148, 1)",  # Magenta
-          "rgba(20, 23, 234, 1)",  # Blue
-          "rgba(234, 173, 20, 1)",  # Orange
-          "rgba(154, 101, 54, 1)",  # Brown
-          "rgba(172, 243, 118, 1)",  # light green
-          "rgba(101, 54, 154, 1)",  # Purple
-          "rgba(172, 243, 118, 1)",  # dark green
-          "rgba(246, 223, 141, 1)",  # Tan
-          "rgba(246, 223, 141, 1)"  # pink
-          ]
+from data.functions import *
 
 logger = logging.getLogger("django")
-
-measurement_units = {
-    "Water": "avg_gallons_per_day",
-    "Electricity": "kWh_usage",
-    "Gas": "therms_usage"
-}
-
-
-def get_years_list_from_data(object_data):
-    """Returns a sorted list of years found across all service dates"""
-    dataset_years = []
-
-    for datapoint in object_data:
-        dataset_years.append(datapoint.service_start_date.year)
-        dataset_years.append(datapoint.service_end_date.year)
-
-    dataset_years = list(set(dataset_years))
-    dataset_years.sort()
-
-    return dataset_years
-
-
-def get_midpoint_of_dates(date1, date2):
-
-    return date1 + (date2 - date1)/2
-
-
-def requested_years_to_use(years_range):
-    """use the years from the request else, use this year and last"""
-    if not years_range:
-        return str(datetime.now().year - 2) + "-" + str(datetime.now().year)
-    return years_range
-
-
-def get_measurements_in_year(all_data, year):
-    """Returns a list of all the measurements within a given year"""
-    this_years_values = []
-
-    # To account for possible service start dates and end dates in different years, use the midpoint
-    for measurement in all_data:
-        if get_midpoint_of_dates(measurement.service_start_date, measurement.service_end_date).year == year:
-            this_years_values.append(measurement)
-
-    return this_years_values
-
-
-def get_YTD_values_for_any_year(year, data):
-    """Get the sum of measurements that have a service date in the year given"""
-    currentMonth = datetime.now().month
-    data_type = type(data[0]).__name__
-
-    if data_type != "CarMiles":
-        measurement_sum = 0
-        all_measurements_in_year = get_measurements_in_year(data, year)
-        for measurement in all_measurements_in_year[:currentMonth - 1]:
-            measurement_sum += getattr(measurement, measurement_units[data_type])
-        return measurement_sum
-    elif data_type == "CarMiles":
-        # VMT is the difference between the most recent reading this year and the one from Jan of this year
-        return CarMiles.objects.get(reading_date__year=year, reading_date__month=currentMonth).odometer_reading \
-                  - CarMiles.objects.get(reading_date__year=year, reading_date__month=1).odometer_reading
-    else:
-        return None
-
-
-def get_YTD_values_for_all_years(data):
-    """Get the sum of measurements that have a service date across all past years
-    Does not include current year"""
-    data_type = type(data[0]).__name__
-
-    if data_type != "CarMiles":
-        years = list(set([x.service_start_date.year for x in data]))
-        years.sort()
-        del years[-1]
-        prev_years_values = [get_YTD_values_for_any_year(year, data) for year in years]
-        return round(get_average(prev_years_values))
-    elif data_type == "CarMiles":
-        years = list(set([a.reading_date.year for a in data]))
-        years.sort()
-        del years[-1]
-        prev_years_values = [get_YTD_values_for_any_year(year, data) for year in years]
-        return round(get_average(prev_years_values))
-    else:
-        return None
-
-
-def get_average(lst):
-    return sum(lst) / len(lst)
 
 
 def home(request):
@@ -133,27 +33,6 @@ def home(request):
                                          "avg_ytd_values": avg_ytd_values})
 
 
-def get_measurement_data(data_name, year, years_range):
-    data_class = apps.get_model(app_label="data", model_name=data_name)
-
-    if year:
-        all_data = data_class.objects.filter(service_start_date__year=year)
-    elif years_range:
-        if "-" in years_range:
-            all_data = data_class.objects.filter(service_start_date__year__gte=years_range.split("-")[0],
-                                                  service_start_date__year__lte=years_range.split("-")[1])
-        elif "," in years_range:
-            # Get all data then remove the ones that don't match these years
-            all_datapoints = data_class.objects.all()
-            all_data = []
-            for y in years_range.split(","):
-                for d in all_datapoints:
-                    if d.service_start_date.year == int(y):
-                        all_data.append(d)
-
-    return all_data
-
-
 def water(request):
     title = "Water"
     measurement = "Average Gallons / Day"
@@ -164,22 +43,7 @@ def water(request):
 
     years = get_years_list_from_data(all_water_data)
 
-    # Template ["label", "color", [["2018, 9", 46.8], ...]]
-    water_line_data = []
-    for count, year in enumerate(years):
-        this_year_water_line_data = []
-        this_year_water_line_data.append(str(year))
-        this_year_water_line_data.append(colors[count])
-
-        water_data = []
-        for water_bill in all_water_data:
-            midpoint_date = get_midpoint_of_dates(water_bill.service_start_date, water_bill.service_end_date)
-            if midpoint_date.year == year:
-                water_data.append([str(midpoint_date.month - 1), water_bill.avg_gallons_per_day])
-
-        this_year_water_line_data.append(water_data)
-
-        water_line_data.append(this_year_water_line_data)
+    water_line_data = create_line_data(years, all_water_data)
 
     return render(request, "page.html", {"title": title,
                                          "measurement": measurement,
@@ -197,22 +61,7 @@ def gas(request):
 
     years = get_years_list_from_data(all_gas_data)
 
-    # Template ["label", "color", [["2018, 9", 46.8], ...]]
-    gas_line_data = []
-    for count, year in enumerate(years):
-        this_year_gas_line_data = []
-        this_year_gas_line_data.append(str(year))
-        this_year_gas_line_data.append(colors[count])
-
-        gas_data = []
-        for gas_bill in all_gas_data:
-            midpoint_date = get_midpoint_of_dates(gas_bill.service_start_date, gas_bill.service_end_date)
-            if midpoint_date.year == year:
-                gas_data.append([str(midpoint_date.month - 1), gas_bill.therms_usage])
-
-        this_year_gas_line_data.append(gas_data)
-
-        gas_line_data.append(this_year_gas_line_data)
+    gas_line_data = create_line_data(years, all_gas_data)
 
     return render(request, "page.html", {"title": title,
                                          "measurement": measurement,
@@ -230,22 +79,7 @@ def electricity(request):
 
     years = get_years_list_from_data(all_elec_data)
 
-    # Template ["label", "color", [["2018, 9", 46.8], ...]]
-    elec_line_data = []
-    for count, year in enumerate(years):
-        this_year_elec_line_data = []
-        this_year_elec_line_data.append(str(year))
-        this_year_elec_line_data.append(colors[count])
-
-        elec_data = []
-        for elec_bill in all_elec_data:
-            midpoint_date = get_midpoint_of_dates(elec_bill.service_start_date, elec_bill.service_end_date)
-            if midpoint_date.year == year:
-                elec_data.append([str(midpoint_date.month - 1), elec_bill.kWh_usage])
-
-        this_year_elec_line_data.append(elec_data)
-
-        elec_line_data.append(this_year_elec_line_data)
+    elec_line_data = create_line_data(years, all_elec_data)
 
     return render(request, "page.html", {"title": title,
                                          "measurement": measurement,
@@ -274,23 +108,7 @@ def car_miles(request):
     for count, o in enumerate(l):
         VMT.append(o-k[count])
 
-    # Create a year, here we add the year and color, not the VMT value just yet
-    # Template ["label", "color", [["2018, 9", 46.8], ...]]
-    car_miles_line_data = []
-    for count, year in enumerate(years):
-        this_year_car_miles_line_data = []
-        this_year_car_miles_line_data.append(str(year))
-        this_year_car_miles_line_data.append(colors[count])
-
-        car_miles_data = []
-        for reading in all_CarMiles_data:
-            if reading.reading_date.year == year:
-                # Will replace the 0 with the VMT value in the next for loop
-                car_miles_data.append([str(reading.reading_date.month - 1), 0])
-
-        this_year_car_miles_line_data.append(car_miles_data)
-
-        car_miles_line_data.append(this_year_car_miles_line_data)
+    car_miles_line_data = create_line_data(years, all_CarMiles_data)
 
     # Need to remove the last month
     car_miles_line_data[-1][2].pop()
